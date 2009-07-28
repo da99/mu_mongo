000c94976bea95b47d1bfa1b3f65d5465bcf74f6
Diego Alban <da01@diegoalban.com>
2009-07-28 14:45:29 -0400

    Minor test.

--- megauni.rb
+++ megauni.rb
@@ -2,12 +2,13 @@ $KCODE = 'UTF8'
 
 require 'rubygems'
 require 'sinatra'
-require 'markaby'
+
 require 'pow'
+
 
 configure do
 
-  Markaby::Builder.set(:indent, 2) 
+  # Markaby::Builder.set(:indent, 2) 
 
   error do
     File.read( Pow!('public/error.html')  ) 
@@ -22,11 +23,10 @@ end
 
 
 get '/' do
-
-  mab  = Markaby::Builder.new( {} )
-  mab.instance_eval(  File.read( Pow!(  'views/index.mab' ) )  ).to_s
+  "test is done"
+  # mab  = Markaby::Builder.new( {} )
+  # mab.instance_eval(  File.read( Pow!(  'views/index.mab' ) )  ).to_s
 
 end
-
 
 
