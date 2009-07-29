93ec25ceac1a3c8caa16f5da0a5f381508400618
Diego Alban <da01@diegoalban.com>
2009-07-29 06:02:19 -0400

    Made database connection code more succinct.

--- secret_closet.rb
+++ secret_closet.rb
@@ -1,20 +1,14 @@
 require 'sequel'
 require 'sequel/extensions/inflector'
 
-class SecretCloset
-    def self.connect
-        return @db_conn if @db_conn
-        @env ||= Object.const_defined?(:Sinatra) ?
+DB = begin
+        env ||= Object.const_defined?(:Sinatra) ?
                         Sinatra::Application.environment :
                         :development;
-        
-               
-        @db_conn = case @env
-          when :test, :development
-
-              db_connection_string = ( @env == :development ) ?
-                                      'postgres://da01:xd19yzxkrp10@localhost/newsprint-db' :
-                                      'postgres://da01:xd19yzxkrp10@localhost/newsprint-db-test' ;
+        case env
+          when :test
+              Sequel.connect 'postgres://da01:xd19yzxkrp10@localhost/newsprint-db-test'
+          when :development
               # === Setup logger
               require 'logger'  
               new_file = Pow( File.expand_path('~/sequel_log.txt') )
@@ -22,17 +16,11 @@ class SecretCloset
               new_logger = {:loggers=> [ Logger.new(new_file) ]}
               
               # Finally...
-              Sequel.connect( db_connection_string  ,  new_logger )
+              Sequel.connect( 'postgres://da01:xd19yzxkrp10@localhost/newsprint-db'  ,  new_logger )
               
           when :production
               Sequel.connect( ENV['DATABASE_URL'] )         
           else
-            raise ArgumentError, "#{$env.inspect} - is not a valid environment for database connection."
+            raise ArgumentError, "#{env.inspect} - is not a valid environment for database connection."
         end
-        
-    end # === def self.connect
-          
-    
 end # === SecretCloset
-
-DB = SecretCloset.connect
