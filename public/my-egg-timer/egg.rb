36d168c0ec03b43b8e02f83304d4605362af608f
da01tv <diego@megauni.com>
2009-08-24 20:59:33 -0400

    Added myeggtimer app.

---
+++ public/my-egg-timer/egg.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'sinatra'
+
+
+configure do
+  
+  error do
+    %~
+    <html>
+        <head>
+          <title>Error</title>
+        </head>
+        <body>
+          <h1>:{</h1>
+          <p>Something went wrong. Come back later when it gets fixed.</p>
+        </body>
+    </html>
+    ~
+  end # error -------------------------------------------------------------------
+
+  not_found do
+    %~
+        <html>
+          <head>
+            <title>Error</title>
+          </head>
+          <body>
+            <h1>:P</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+          </body>
+        </html>
+    ~  
+  end # not_found ----------------------------------------------------------------
+  
+end
+
+
+# get '/' do
+#  File.read( File.expand_path( File.join(File.dirname(__FILE__), 'public/index.html' ) ) ) 
+# end
+
+get '/' do
+  File.read( File.expand_path('./public/main_index.html') )
+end
+
+
+
