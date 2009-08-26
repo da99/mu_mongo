0bb46e87247ddc04c4753702ccd79373c8b91386
diego <diego@miniuni.com>
2009-08-26 13:36:54 -0400

    Minor touches/experiment with /hearts section.

--- actions/heart.rb
+++ actions/heart.rb
@@ -1,7 +1,11 @@
 get '/hearts' do
 
   describe :heart, :show
-  @hearts = [News.first]
+  @hearts = News.reverse_order(:created_at).limit(10).all
   render_mab
 
+end
+
+get '/media/heart_links/images/*' do
+  redirect( 'http://surferhearts.s3.amazonaws.com/heart_links' + File.join('/', params['splat'] ))
 end
