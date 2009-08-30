e4b32c1bbcaabb96138850b2c0e0ae64bbca9252
da01tv <diego@megauni.com>
2009-08-30 18:21:00 -0400

    Dev. checkpoint for blato task: new_computer:start

--- helpers/blato/new_computer.blato.rb
+++ helpers/blato/new_computer.blato.rb
@@ -1,10 +1,12 @@
 class NewComputer
+  
   include Blato
+
+
   bla  :start, "Prints out info. and checks installed gems. Safe to run multiple times."  do
     shout "Install bzr: https://launchpad.net/~bzr/+archive/ppa", :white
     shout "Install git: ", :white    
     
-    shout "Install mercurial: https://launchpad.net/~mercurial-ppa/+archive/stable-snapshots", :white
     
     shout "Firefox: browser.history_expire_days.mirror && browser.history_expire_days to 3: http://blogs.n1zyy.com/n1zyy/2008/09/16/firefoxs-history-setting/", :white
     shout "Firefox: browser.history_expire_sites to '400'", :white
@@ -15,7 +17,7 @@ class NewComputer
 username = %s <%s>      
       ~.strip % [MY_NAME, MY_EMAIL] )
     }
-    shout capture('git config --global user.name %s' % MY_NAME.inspect)
+    shout capture('git config --global user.name %s' % MY_NAME.inspect) 
     shout capture('git config --global user.email %s' % MY_EMAIL)
     shout capture('bzr whoami "%s <%s>"' % [MY_NAME, MY_EMAIL])
     shout %~ 
@@ -24,63 +26,14 @@ username = %s <%s>
              Replace 'x' with the latest Postgresql version you are using. 
              If it still does not work: try using the 'postgres' gem instead. 
              More info: http://rubyforge.org/projects/ruby-pg 
-          ~ 
+          ~ if !capture('psql --version')['psql (PostgreSQL) 8']
     shout %~ 
             Always show hidden files in  Nautilus: 
             http://www.watchingthenet.com/always-show-hidden-files-in-ubuntu-nautilus-file-browser.html
             Alt+F2 > gconf-editor > "desktop / gnome / file_views"
     ~, :white
     
-    shout "Installing VIM settings.", :white
-    vim_bk = Pow('~/.vim-tmp')
-    if !vim_bk.exists?
-        shout "Creating #{vim_bk}", :yellow
-        shout capture("mkdir %s" % vim_bk.to_s), :white
-    end
-    Pow(File.expand_path('~/.vimrc')).create do |f|
-      f.puts <<-EOF
-colorscheme vividchalk
-set tabstop=4
-set shiftwidth=4
-set autoindent
-set expandtab
-set smarttab
 
-set number
-set hlsearch
-syntax on
-
-if has("gui_running")
-    set guifont=Liberation\ Mono\ 14
-endif
-
-autocmd FileType make     set noexpandtab
-autocmd FileType python   set noexpandtab
-
-let g:fuzzy_ignore = "*.log"
-let g:fuzzy_matching_limit = 70
-
-map <leader>f :FuzzyFinderFile<CR>
-map <leader>b :FuzzyFinderBuffer<CR>
-map <leader>d :execute 'NERDTreeToggle ' . getcwd()<CR>
-
-
-set backupdir=~/.vim-tmp,~/.tmp,~/tmp,/var/tmp,/tmp
-set directory=~/.vim-tmp,~/.tmp,~/tmp,/var/tmp,/tmp
-
-EOF
-# From: http://weblog.jamisbuck.org/2008/11/17/vim-follow-up
-# From: http://items.sjbach.com/319/configuring-vim-right
-    end
-    
-    bash_file = Pow(File.expand_path('~/.bashrc')).read
-    if !bash_file['gvim --remote-tab-silent']
-        shout('Put alias gvim="gvim --remote-tab-silent" at the end of your ~/.bashrc file.')
-    end
-    
-    shout( "Install vividchalk in ~/.vim/colors", :white ) if !Pow('~/.vim/colors/vividchalk.vim').exists?
-    shout "Installing gems using task: gems:update", :yellow
-    shout capture_task('gems:update'), :white
     
     if !capture('which blato')['bin/blato']
       shout "Symlink blato to a path dir."
@@ -90,54 +43,95 @@ EOF
       shout "18 * * * * #{capture('which blato')} bzr:quiet_my_life_dev_check", :white 
     end
     
-    irb_rc = Pow("~/.irbrc")
-    irb_rc_read = %~
-begin
-  require 'rubygems'
-  require 'wirble'
+    install_vim
 
-  # start wirble (with color)
-  Wirble.init
-  Wirble.colorize
-rescue LoadError => err
-  warn "Couldn't load Wirble: \#\{err\}"
-end
-    ~
-    irb_match = File.read(irb_rc).strip == irb_rc_read.strip
-    if irb_rc.exists? &&   irb_rc.file? && !irb_match
-      shout "DELETE file: #{irb_rc}", :red
-    else
-      if !irb_rc.exists? && !irb_match
-        shout `gem install wirble` if !capture('gem list')['wirble']
-        irb_rc.create { |f| 
-          f.puts irb_rc_read
-        }
-      end
+    install_irb
+
+    install_light_conf
+
+    install_gemrc
+
+  end # === bla :start
+
+  def install_vim
+    shout "Installing VIM settings.", :white
+    vim_bk = Pow('~/.vim-tmp')
+    if !vim_bk.exists?
+        shout "Creating #{vim_bk}", :yellow
+        shout capture("mkdir %s" % vim_bk.to_s), :white
+    end
+
+    vimrc = (MY_PREFS / 'vim/vimrc.vim')
+    new_vim_rc = Pow('~/.vimrc')
+    if !new_vim_rc.exists?
+        shout(capture('ln -s %s %s' % [vimrc.to_s.inspect, new_vim_rc.to_s.inspect] ))
+    end
+
+    bash_file = Pow(File.expand_path('~/.bashrc')).read
+    if !bash_file['gvim --remote-tab-silent']
+        shout('Put alias gvim="gvim --remote-tab-silent" at the end of your ~/.bashrc file.')
     end
     
+    shout( "Install vividchalk in ~/.vim/colors", :white ) if !Pow('~/.vim/colors/vividchalk.vim').exists?
+
+  end
+
+  def install_irb
+    
+    home_irb = Pow(File.expand_path("~/.irbrc"))
+    irb = (MY_PREFS / 'ruby' / 'irbrc.rb')
+    
+    both_exist = home_irb.exists? && irb.exists?
+    both_match = both_exist && (home_irb.read.strip == irb.read.strip) &&  File.symlink?(home_irb.to_s)
+
+    
+    install_irb = !both_exist && irb.exists?
+
+    if both_exist && !both_match 
+        shout "DELETE file: #{home_irb}"
+    end
+    
+    if install_irb
+        shout `gem install wirble` if !capture('gem list')['wirble']
+        shout(capture("ln -s %s %s" % [irb.to_s.inspect, home_irb.to_s.inspect]))
+    end
+  end
+
+  def install_gemrc
     gem_rc_yaml = (MY_PREFS / 'ruby' / 'gemrc.yaml')
     gem_rc = Pow('~/.gemrc')
-    do_link = gem_rc_yaml.exists? && !gem_rc.exists?
+    both_exists = gem_rc_yaml.exists? && gem_rc.exists?
+    both_match = both_exists && gem_rc_yaml.read.strip == gem_rc.read.strip && File.symlink?(gem_rc.to_s)
+    do_link = gem_rc_yaml.exists? && !both_exists 
 
     if do_link
-        content_gem_rc = gem_rc_yaml.read
-        shout capture('ln -s %s %s' % [ gem_rc.to_s.inspect, gem_rc_yaml.to_s.inspect ] )
+        shout capture('ln -s %s %s' % [gem_rc_yaml.to_s.inspect,  gem_rc.to_s.inspect] )
     else
         shout "#{gem_rc_yaml} not found." if !gem_rc_yaml.exists?
-        shout "#{gem_rc} must be deleted." if gem_rc.exists?
+        shout "#{gem_rc} must be deleted." if both_exists && !both_match 
+    end
+    shout "Installing gems using task: gems:update", :yellow
+    shout capture_task('gems:update'), :white
+  end
+
+  def install_light_conf
+
+    light_conf = (MY_PREFS / 'light.conf')
+    sudo_light_conf = Pow("/etc/lighttpd/lighttpd.conf")
+
+    if !light_conf.exists?
+        shout "File not found: #{light_conf}"      
     end
     
-    light_conf = (MY_PREFS / 'ligttpd.conf')
-    sudo_light_conf = ("/etc/lighttpd/lighttpd.conf")
+    both_exist = light_conf.exists? && sudo_light_conf.exists?
+    both_match = both_exist && ( light_conf.read.strip != sudo_light_conf.read.strip )
 
-
-    shout "File not found: #{light_conf}"  if !light_conf.exists?
-    both_files_exist = light_conf.exists? && sudo_light_conf.exists?
-
-    if both_files_exists && light_conf.read.strip != sudo_light_conf.read.strip
-        shout("Sudo copy file to #{sudo_light_conf}", :red) 
+    if both_exist && !both_match
+        shout( "Sudo copy file to #{sudo_light_conf}" ) 
     end
     
   end
 
-end
+
+
+end # === class NewComputer
