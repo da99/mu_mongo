5864c2478f744cd03cc148670b724a52117d24ff
da01tv <diego@megauni.com>
2009-09-06 00:49:00 -0400

    Improved /help section.

--- views/skins/jinx/main_help.rb
+++ views/skins/jinx/main_help.rb
@@ -15,22 +15,45 @@ partial('__nav_bar')
 div.content!  { 
   ul.faq {
   
-    li {
+     li {
       div.question "What is MegaUNi.com?"
       div.answer {
-        span "Present: Messy. "
-        br
-        span "Near future: Unification of various ideas."
-        br
-        span "Future: I'm not sure."
-        br
-        span "Always: No ads. No spam. No marketing."
+        span "A unification of past ideas/websites I've worked on."
+      }
+    }
+
+    li {
+      div.question "I'm worried about the economy."
+      div.answer {
+        span "You should be. It's a Depression, not a Recession."
       }
     } 
-    
-
   
-    
+    li {
+      div.question "I'm old. What should I do?"
+      div.answer {
+        span "Buy: "
+        a('Longevinex Advantage.', :href=>'http://www.LongevinexAdvantage.com/')
+        br 
+        span "For: Sleep, memory loss, rheumatoid arthritis, osteo-arthritis, cancer, energy, diabetes.
+        Yes. I'm serious. Everyone I know who has tried it has shown improvement in those conditions 
+        and 90%-100% pain loss."
+        
+      }
+    } 
+
+
+    li {
+      div.question "Are there better alternatives to anti-depressants?"
+      div.answer {
+        span "First, stop seeing the idiot doctor who prescribes useless, harmful anti-depressants."
+        span "Second, take "
+        a("Methyl-B12 and Folic acid.", :href=>'http://www.google.com/search?q=methyl-b12+folic+acid+depression')
+        span '(via '
+        a('The Natural Health Librarian.', :href=>'http://www.naturalhealthlibrarian.com/')
+        span ' Buy their e-books. Great stuff.)'
+      }
+    }      
      
   } # === ul
 } # == div.content!
