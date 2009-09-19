8ce8315bb3f1bbe83c11c28fe99614b33a062c15
da01tv <diego@megauni.com>
2009-09-19 16:39:06 -0400

    Typos fixes in last migration.

--- migrations/018_alter_logins_to_log_ins.rb
+++ migrations/018_alter_logins_to_log_ins.rb
@@ -1,7 +1,8 @@
 class AlterLoginsToLogIns_18 < Sequel::Migration
 
   def up  
-    rename_table :login_attempts, :log_in_attempts )   end
+    rename_table :login_attempts, :log_in_attempts 
+  end
 
   def down
     drop_table(:log_in_attempts) if table_exists?(:log_in_attempts)
