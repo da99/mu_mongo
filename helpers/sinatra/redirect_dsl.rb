9290035606e1972906c4ec98e7f892c4f5e76d25
da01tv <diego@miniuni.com>
2009-11-22 09:31:44 -0500

    Fixes for RedirectDSL.

--- helpers/sinatra/redirect_dsl.rb
+++ helpers/sinatra/redirect_dsl.rb
@@ -1,4 +1,5 @@
 def redirect &blok
+
   RedirectDSL.new &blok 
 end
 
@@ -7,29 +8,45 @@ configure do
 
   class RedirectDSL
   
-    private
-      attr_writer :from, :to
-    
-    def inititalize &blok
+    def initialize &blok
+
       instance_eval &blok
+      
       if !@from || !@to
         raise ArgumentError, "Both need to be set: :to, :from (#{@to.inspect}, #{@from.inspect})"
       end 
+      
       @from.each { |f|
-        self.class.instance_eval %~
+            
+        line = __LINE__
+        code = %~
          get #{f.inspect} do 
           redirect #{@to.inspect}
          end
         ~
+        self.class.instance_eval code, __FILE__, line
+
       }
+
     end
 
     def from *args
-      @from= args.flatten
+      @from = args.flatten.map { |f|  normalize_path(f) }
     end
     
     def to path_or_regex
-      @to = path_or_regex
+      @to = normalize_path(path_or_regex)
+    end
+
+    private # =====================================
+
+    def normalize_path path
+      case path
+        when Symbol
+          "/#{path}/"
+        else
+          path
+      end
     end
 
   end # === RedirectDSL
