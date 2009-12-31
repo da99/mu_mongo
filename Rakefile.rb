397bdb17a75d10400c11346fb2017ecff1f25873
da01 <diego@miniuni.com>
2009-12-31 15:12:42 -0500

    Minor improvements to Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,20 +3,20 @@
 require 'term/ansicolor'
 
 def puts_white msg
-  Term::ANSIColor.send(:white) { msg }
+  puts( 
+    Term::ANSIColor.send(:white) { msg } 
+  )
 end
 
 def puts_red msg
-  Term::ANSIColor.send(:red) { msg }
+  puts( 
+    Term::ANSIColor.send(:red) { msg } 
+  )
 end
 
 def assert_not_empty raw_val
   val = raw_val.is_a?(String) ? raw_val.strip : raw_val;
-
-  if val.nil? || val.empty?
-    raise "#{raw_val.inspect} must not be empty"
-  end
-
+  raise "#{raw_val.inspect} must not be empty" if val.nil? || val.empty?
   val
 end
 
