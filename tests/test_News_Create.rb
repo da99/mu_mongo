a764d94f44a433680a49a16b896424a35e4c5ccf
da01 <diego@miniuni.com>
2010-01-02 23:26:35 -0500

    Tests for News_Create all pass.

--- tests/test_News_Create.rb
+++ tests/test_News_Create.rb
@@ -3,19 +3,98 @@ require 'tests/__helper__'
 
 class News_Create < Test::Unit::TestCase
 
-	must 'only allow Admins as creators'
+	must 'only allow Admins as creators' do
+    assert_raise(Couch_Plastic::Unauthorized_Creator) do
+      News.create(regular_user, {})
+    end
+  end
 
-  must 'require title'
+  must 'require title' do
+    doc = begin
+            News.create( 
+              admin_user, 
+              :club   =>'club-hearts',
+              :title  => nil,
+              :teaser => 'My Teaser.',
+              :body   => 'The Body',
+              :published_at => nil
+             )
+          rescue News::Invalid => e
+            e.doc
+          end
 
-  must 'require body'
+    assert_equal(
+      "Title is required.", 
+      doc.errors.first
+    )
+  end
 
-  must 'change published_at to datetime'
+  must 'require body' do
+    doc = begin
+            News.create( 
+              admin_user, 
+              :club   =>'club-hearts',
+              :title  => 'My News',
+              :teaser => 'My Teaser.',
+              :body   => nil,
+              :published_at => nil
+             )
+          rescue News::Invalid => e
+            e.doc
+          end
 
-  must 'set published_at to now as a default'
+    assert_equal(
+      "Body is required.", 
+      doc.errors.first
+    )
+  end
 
-  must 'split tag string on new lines'
+  must 'set published_at to now as a default' do
+    doc = News.create( 
+      admin_user, 
+      :club   =>'club-hearts',
+      :title  => 'My News',
+      :teaser => 'My Teaser.',
+      :body   => 'My body.',
+      :published_at => nil
+    )
 
-  must 'strip each tag'
+    assert_equal(
+      chop_last_2(utc_string), 
+      chop_last_2(doc.data.published_at)
+    )
+  end
+
+  must 'split tag string on new lines' do
+    doc = News.create( 
+      admin_user, 
+      :club   =>'club-hearts',
+      :title  => 'My News',
+      :teaser => 'My Teaser.',
+      :body   => 'My body.',
+      :tags   => "t1\nt2\nt3",
+      :published_at => nil
+    )
+
+    assert_equal(
+      %w{ t1 t2 t3 }, 
+      doc.data.tags.sort
+    )
+  end
+
+  must 'strip each tag' do 
+    doc = News.create( 
+      admin_user, 
+      :club   =>'club-hearts',
+      :title  => 'My News',
+      :teaser => 'My Teaser.',
+      :body   => 'My body.',
+      :tags   => "t1  \n \n \nt2  \n  \n  t3",
+      :published_at => nil
+    )
+
+    assert_equal %w{ t1 t2 t3 }, doc.data.tags.sort
+  end
 
   must 'reject any empty tag' do
     doc = News.create( 
