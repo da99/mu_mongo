3c8af692fc61b686f0fe42174be227ea75fcbff9
da01 <diego@miniuni.com>
2010-03-30 09:43:29 -0400

    Development checkpoint. (Mustache/css compilation.)

--- helpers/couchdb_views/messages_public.js
+++ helpers/couchdb_views/messages_public.js
@@ -4,7 +4,7 @@
 
 function(doc) {
 
-  if (doc.data_model == 'Message' && doc.privacy == 'public' )
+  if (doc.data_model == 'Message' && doc.privacy == 'public' && doc.created_at && doc.created_at > '2010-01-01 00:00:00')
     emit( doc.created_at, 1 );
 
 };
